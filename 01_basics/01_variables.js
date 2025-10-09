c2d31c80bd80fef0d3b6b90a18f2395efb7f0f00
Deepanshu <deepanshugoyal1264@gmail.com>
2025-10-09 12:42:48 +0000

    1.variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=674667
+let accountEmail="fbkjf@gmail.com"
+var accountPassword="12345"
+accountCity="Barnala"
+accountEmail="deep@gmai.com"
+accountPassword="34234"
+accountCity="Rajpura"
+let accountState;
+
+/*
+donot use var bcz of scope anf functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
